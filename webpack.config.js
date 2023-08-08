f4646e8b9d4f66d409a4b28acb16470141266f28
Andresr35 <andresjruiz@cpp.edu>
2023-08-08 14:12:21 -0700

    Set up basic files for project

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    mode:'development',
+  entry: './src/index.js',
+  devtool:'inline-source-map',
+  devServer:{
+    static:'./dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            esModule: false,
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs"
+          }
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            esModule: false,
+            name: "[name].[hash].[ext]",
+            outputPath: "fonts"
+          }
+        }  
+      }      
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+        title:'To Do List',
+        template:'./src/index.html'
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean:true,
+  },
+  optimization:{
+    runtimeChunk:'single',
+  },
+
+};
