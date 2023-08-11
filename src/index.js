8430fc777690b7b1899421b0f362f403eccf9d4b
Andresr35 <andresjruiz@cpp.edu>
2023-08-11 16:15:00 -0700

    combine lists and reminders to create groups, and add css to nav components

--- src/index.js
+++ src/index.js
@@ -7,45 +7,117 @@ const addList = document.querySelector(".addList");
 const addReminder = document.querySelector(".addReminder");
 const main = document.querySelector("main");
 const listDiv = document.querySelector(".lists");
+const reminderDiv = document.querySelector(".listContent");
 
 const lists = [];
 
-
 const List = (name) => {
-    let quantity = 0;
- const reminders = [];
- const pushReminder =(reminder)=>reminders.push(reminder);
+  const reminders = [];
+  let quantity = 0;
+  const div = document.createElement("div");
+  div.id = "list" + name;
+  div.innerHTML = name;
+
+  const pushReminder = (reminder) => {
+    reminders.push(reminder);
+    quantity++;
+    quantityElement.innerHTML = quantity;
+  };
   const getName = () => name;
-  const getQuantity = ()=>quantity;
-  
-  return { getName,getQuantity,pushReminder};
+  const getQuantity = () => quantity;
+  const quantityElement = document.createElement("div");
+  quantityElement.innerHTML = quantity;
+  div.appendChild(quantityElement);
+
+  listDiv.appendChild(div);
+
+  const makeDefault = () => (div.className = "defaultList");
+  const removeDefault = () => (div.className = "");
+
+  const getDiv = () => div;
+
+  const renderReminders = () => {
+    reminders.forEach((reminder) => reminder.render());
+  };
+  const removeReminders = () => {
+    reminderDiv.replaceChildren();
+  };
+
+  return {
+    getName,
+    getQuantity,
+    getDiv,
+    pushReminder,
+    makeDefault,
+    removeDefault,
+    renderReminders,
+    removeReminders,
+  };
 };
 
-let currentList = List("Default");
-lists.push(currentList);
+const Reminder = (name, date) => {
+  const getName = () => name;
+  const getDate = () => date;
+  const div = document.createElement("div");
+  div.innerHTML = name + " " + date;
+
+  const render = () => reminderDiv.appendChild(div);
+  render();
+
+  return { getDate, getName, render };
+};
 
 addList.addEventListener("click", () => {
-  main.appendChild(addListForm);// add create list form to page
-  addListForm.addEventListener("submit", (event) => {
-    event.preventDefault();// Stop page from refreshing
-    const name = event.target[0].value; 
-    const newlist = List(name);
-    lists.push(newlist);
-    const div = listDiv.appendChild(document.createElement("div"));
-    div.id = "list" + name;
-    div.innerHTML = name;
-    const quantity = document.createElement('div');
-    quantity.innerHTML = newlist.getQuantity();
-    div.appendChild(quantity);
-    addListForm.reset();
-    addListForm.remove();
+  main.appendChild(addListForm); // add create list form to page
+  addListForm.addEventListener(
+    "submit",
+    (event) => {
+      event.preventDefault(); // Stop page from refreshing
+      const name = event.target[0].value;
+      addToList(List(name));
+      addListForm.reset();
+      addListForm.remove();
+    },
+    { once: true }
+  );
+});
+
+/**
+ * This adds a list item to the array of lists and pushes it to the DOM
+ *
+ * @param   {List}  list  list you want to push
+ *
+ * @return  {undefined}        undefined
+ */
+const addToList = (list) => {
+  lists.push(list);
+  list.getDiv().addEventListener("click", () => {
+    setDefaultList(list);
+  });
+};
+
+addReminder.addEventListener("click", () => {
+  main.appendChild(addReminderForm);
+  addReminderForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const name = event.target[0].value;
+    // const date = new Date(event.target[1].value);
+    const date = event.target[1].value;
+
+    currentList.pushReminder(Reminder(name, date));
+    addReminderForm.reset();
+    addReminderForm.remove();
   },{once:true});
 });
 
-addReminder.addEventListener("click",()=>{
-    main.appendChild(addReminderForm);
-    addReminderForm.addEventListener("submit",()=>{
+const setDefaultList = (list) => {
+  currentList.removeDefault();
+  currentList.removeReminders();
+  currentList = list;
+  list.makeDefault();
+  list.renderReminders();
+};
 
-        
-    })
-})
+let currentList = List("Default");
+addToList(currentList);
+setDefaultList(currentList);
